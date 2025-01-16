768c741b63b5e85299b88e0fa586a7c31b6f545a
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-01-16 14:45:20 +0100

    execution of statements

--- tests/test_interpreter.c
+++ tests/test_interpreter.c
@@ -2,34 +2,22 @@
 #include "parser.h"
 #include "interpreter.h"
 
-int main(void){
-    const char* source = "2+(2.5*2-10)";
+int main(){
+    const char* source = "print(\"Hello, World!\")";
     Scanner* scanner;
     Parser *parser;
-    Expr* final_expr;
+    List_Stmt* stmts;
 
     scanner = newScanner(source);
     scanTokens(scanner);
     parser = newParser(scanner->tokens);
 
-    final_expr = parse(parser);
+    stmts = parse(parser);
 
-    Literal* result = Interpret(final_expr);
-    switch (result->type){
-        case C_INT:
-            printf("%d\n", *((int*)result->data));
-            break;
-        case C_DOUBLE:
-            printf("%lf\n", *((double*)result->data));
-            break;
-        default:
-            fprintf(stderr, "Invalid type for result\n");
-            exit(1);
-    }
+    Interpret(stmts);
 
     freeScanner(scanner);
     freeParser(parser);
-    freeExpr(final_expr);
-    freeLiteral(result);
+    freeList_Stmt(stmts);
     return 0;
 }
