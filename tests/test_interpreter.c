88886e92602b9b9150887ef020e0cb35806a82e8
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-01-17 22:26:29 +0100

    updated everything to work with variables, and multiple lines

--- tests/test_interpreter.c
+++ tests/test_interpreter.c
@@ -1,23 +1,33 @@
 #include "scanner.h"
 #include "parser.h"
 #include "interpreter.h"
+#include "types.h"
 
 int main(){
-    const char* source = "print(\"Hello, World!\")";
+    const char* sources[] = {"y=2+3*2","x=y*y","print(x)",NULL};//,"print(x)",NULL, "z=x+5", "print(z)",NULL};
+    const char** sources_ptr = sources;
     Scanner* scanner;
     Parser *parser;
     List_Stmt* stmts;
+    types_init();    
+    interpreter_init();
+    
+    while (*sources_ptr != NULL){
+        scanner = newScanner(*sources_ptr);
+        scanTokens(scanner);
 
-    scanner = newScanner(source);
-    scanTokens(scanner);
-    parser = newParser(scanner->tokens);
+        parser = newParser(scanner->tokens);
 
-    stmts = parse(parser);
+        stmts = parse(parser);
 
-    Interpret(stmts);
+        Interpret(stmts);
 
-    freeScanner(scanner);
-    freeParser(parser);
-    freeList_Stmt(stmts);
+        freeScanner(scanner);
+        freeParser(parser);
+        freeList_Stmt(stmts);
+        sources_ptr++;
+    }
+    freeInterpreter();
+    free_types();
     return 0;
 }
