fd0ae68a06d6e42c5adf70a6ed619ade71cf68d0
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-01-22 11:47:26 +0100

    created for utility purposes

---
+++ include/forward_decl.h
@@ -0,0 +1,14 @@
+#ifndef FORWARD_DECL_H
+#define FORWARD_DECL_H
+
+typedef struct key_field key_field;
+
+typedef struct Rtype Rtype;
+typedef struct Literal Literal;
+typedef struct List_Rtype List_Rtype;
+typedef struct Rvariable Rvariable;
+typedef struct List_var List_var;
+typedef struct ht_var HT_var;
+typedef struct ht_Rtype HT_Rtype;
+
+#endif // FORWARD_DECL_H
