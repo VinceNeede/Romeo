2e252048656d419a3f76c4de62c6a3a93c8549e6
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-01-22 11:48:34 +0100

    added look up tables

--- include/Rtype.h
+++ include/Rtype.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include "LinkList_Rtype.h"
+#include "HT_var.h"
 
 typedef struct List_string List_string;
 void freeList_string(List_string *);
@@ -12,7 +14,7 @@ typedef enum{
     FUNCTION
 } key_type;
 
-typedef struct {
+typedef struct key_field{
     key_type type;
     union{
         char* name;
@@ -29,10 +31,13 @@ typedef struct Rtype{
     char * name;
     size_t size;
     int is_callable;
+    HT_var ** look_up_table;
 } Rtype;
 
 Rtype *newRtype(key_type key_type, char *name, size_t size, int is_callable);
 void freeRtype(Rtype *type);
 Rtype *copyRtype(Rtype *type);
 void free_key_field(key_field *key);
+size_t hash_fun(const key_field* key);
+int cmp_fun(const key_field* ht_key, const key_field* cmp_key);
 #endif // RTYPE_H
--- lib/Rtype.c
+++ lib/Rtype.c
@@ -1,5 +1,32 @@
 #include "Rtype.h"
+#include "LinkList_string.h"
 
+#define LOOKUP_TABLE_SIZE 23
+
+size_t hash_fun(const key_field* key){
+    const char * key_str = (const char*)key->field.function.name;
+    size_t hash = 0;
+    for (int i = 0; key_str[i] != '\0'; i++){
+        hash = 31 * hash + key_str[i];
+    }
+    return hash;
+}
+int cmp_fun(const key_field* ht_key, const key_field* cmp_key){
+    int cmp_names = strcmp((const char*)ht_key->field.function.name, (const char*)cmp_key->field.function.name);
+    if (cmp_names) return 0;
+    List_string *args1 = ht_key->field.function.args_types;
+    List_string *args2 = cmp_key->field.function.args_types;
+    if (ht_key->field.function.non_optional_args > args2->size) return 0;
+    Node_string *current1 = args1->head;
+    Node_string *current2 = args2->head;
+    int i = 0;
+    while (current2 != NULL && !strcmp(current1->data, current2->data)){
+        current1 = current1->next;
+        current2 = current2->next;
+        i++;
+    }
+    return i >= ht_key->field.function.non_optional_args;
+}
 
 Rtype *newRtype(key_type key_type, char *name, size_t size, int is_callable){
     Rtype *type = (Rtype*)malloc(sizeof(Rtype));
@@ -7,12 +34,20 @@ Rtype *newRtype(key_type key_type, char *name, size_t size, int is_callable){
     type->name = strdup(name);
     type->size = size;
     type->is_callable = is_callable;
+    if (key_type == NAME){
+        type->look_up_table = (HT_var**)malloc(sizeof(HT_var*));
+        *(type->look_up_table) = newHT_var(LOOKUP_TABLE_SIZE, hash_fun, cmp_fun, NULL, freeRvariable);
+    } else type->look_up_table = NULL;
     return type;
 }
 
 void freeRtype(Rtype *type){
     if (type == NULL) return;
     free(type->name); 
+    if (type->look_up_table != NULL){
+        freeHT_var(*(type->look_up_table));
+        free(type->look_up_table);
+    }
     free(type);
 }
 void free_key_field(key_field *key){
