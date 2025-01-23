3f1398180a6b868e1259f88968c88c2ccc74af73
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-01-23 08:16:59 +0100

    many things are moved to memblock. Redefined how a variable gets read as literal (a copy is created)

--- include/variable.h
+++ include/variable.h
@@ -3,20 +3,9 @@
 #include "forward_decl.h"
 #include "types.h"
 #include "token.h"
+#include "memblock.h"
 
-typedef struct Rvariable{
-    char* type;
-    key_field *key;     // can become a token
-    void *pos;
-} Rvariable;
 
-Rvariable *newRvariable(char* type, key_field* key, void *pos);
-// Rvariable *newRvariable_from_Literal(key_field* key, const Literal* lit);
-void freeRvariable(Rvariable *var);
-
-int *get_int_var(Rvariable* var);
-double *get_double_var(Rvariable* var);
-char *get_string_var(Rvariable* var);
 Literal *var_to_literal(Rvariable* var);
 Rvariable *copyvar(Rvariable *var);
 #endif // VARIABLE_H
--- lib/variable.c
+++ lib/variable.c
@@ -2,72 +2,17 @@
 #include "callable.h"
 #include <string.h>
 
-Rvariable *newRvariable(char* type, key_field *key, void *pos){
-    Rvariable *var = (Rvariable*)malloc(sizeof(Rvariable));
-    var->type = strdup(type);
-    var->key = key;
-    var->pos = pos;
-    return var;
-}
-
-// Have to think about how to free the variable
-void freeRvariable(Rvariable *var){
-    // fprintf(stderr, "Not implemented\n");
-    if (var == NULL) return;
-
-    if (var->key != NULL) free_key_field(var->key);
-
-    if (cmp_types(var->type, "string")) free((char*)var->pos);
-    else if (cmp_types(var->type, "int")) free((int*)var->pos);
-    else if (cmp_types(var->type, "double")) free((double*)var->pos);
-    else if (cmp_types(var->type, "function")) freeCallable((Callable*)var->pos);
-    else if (cmp_types(var->type, "bool")) free((int*)var->pos);
-    else fprintf(stderr, "cannot free void*\n");
-
-    if (var->type != NULL) free(var->type);
-
-    free(var);
-}
-
-// Rvariable *newRvariable_from_Literal(const Token* tname, const Literal* lit){
-//     return newRvariable(lit->type, tname, lit->data);
-// }
-
-int *get_int_var(Rvariable* var){
-    return (int*)var->pos;
-}
-
-double *get_double_var(Rvariable* var){
-    return (double*)var->pos;
-}
-
-char *get_string_var(Rvariable* var){
-    return (char*)var->pos;
-}
 
 Literal *var_to_literal(Rvariable* var){
-    void *pos;
-    int free_data = 1;
-    if (cmp_types(var->type, "int") || cmp_types(var->type, "bool")){
-        pos = malloc(sizeof(int));
-        *(int*)pos = *(int*)var->pos;
-    } else if (cmp_types(var->type, "double")){
-        pos = malloc(sizeof(double));
-        *(double*)pos = *(double*)var->pos;
-    } else if (cmp_types(var->type, "string")){
-        size_t len = strlen((char*)var->pos);
-        pos = malloc( len + 1);
-        strcpy((char*)pos, (char*)var->pos);
-        ((char*)pos)[len] = '\0';
-    } else if (cmp_types(var->type, "function")){
-        pos = var->pos;
-        free_data = 0;
-    } else {
-        fprintf(stderr, "Invalid type for variable\n");
-        exit(1);
+    // new memory is allocated and the value is copied
+    if (strcmp(var->type, "string") == 0){
+        return newLiteral(var->type, strdup((char*)var->block->start), 1);
+    } else if (strcmp(var->type, "function") == 0){
+        return newLiteral(var->type, (void*)var->block->start, 0);
     }
-    
-    return newLiteral(var->type, pos, free_data);
+    void *pos = malloc(var->block->size);
+    memcpy(pos, (void*)var->block->start, var->block->size);
+    return newLiteral(var->type, pos, 1);
 }
 
 Rvariable *copyvar(Rvariable *var){
